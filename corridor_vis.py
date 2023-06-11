70cfe6a60556c8a8015bd1625f2142bc4a79b927
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-06-11 22:58:47 +0300

    0014: corridor_vis.py 3d view

--- corridor_vis.py
+++ corridor_vis.py
@@ -10,29 +10,37 @@ IN_PATH = '../input/001_cmp_picking'
 OUT_PATH = '../output/001_zomf_corr'
 
 
-def set_fig_ax(picking, fig, ax):
-    ax.invert_yaxis()
-    ax.set_title(f'Velocity picking\n (x, y) = ({picking[0][0]}, {picking[0][1]})')
-    ax.set_xlabel('Velocity')
-    ax.set_ylabel('Time')
-    fig.set_figwidth(5.5)
+def set_fig_ax(fig, ax):
+    ax.invert_zaxis()
+    ax.set_title(f'Velocity picking')
+    ax.set_xlabel('Count')
+    ax.set_ylabel('Velocity')
+    ax.set_zlabel('Time')
+    fig.set_figwidth(10)
     fig.set_figheight(8)
-    return fig, ax
 
 
-def plot_corridor(input_name):
+def plot_corridor_3d(input_name):
     df = pd.read_csv(input_name, sep='\t', skiprows=13, usecols=np.arange(4),
                      dtype=np.float64, header=0, names=['x', 'y', 'v', 't'])
 
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    count = 0
+    set_fig_ax(fig, ax)
+
     for group in df.groupby(by=['x', 'y']):
-        fig, ax = set_fig_ax(group, *plt.subplots())
-        ax.plot(group[1][['v']], group[1][['t']], marker='o')
-        plt.show()
+        x = group[1][['v']].to_numpy()
+        x.fill(count)
+        ax.plot(x, group[1][['v']], group[1][['t']], marker='o')
+        count += 1
+
+    plt.show()
 
 
 def run():
     for name in create_file_list(IN_PATH):
-        plot_corridor(join(IN_PATH, name))
+        plot_corridor_3d(join(IN_PATH, name))
 
 
 if __name__ == '__main__':
--- sandbox.py
+++ sandbox.py
@@ -1,15 +1,25 @@
-import numpy as np
-from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
 
-plt.rcParams["figure.figsize"] = [7.00, 3.50]
-plt.rcParams["figure.autolayout"] = True
-fig, axs = plt.subplots(1, 1)
 
-data = np.random.random((10, 3))
-columns = ("Column I", "Column II", "Column III")
-axs.axis('tight')
-axs.axis('off')
+fig, axs = plt.subplots(1, 3, subplot_kw={'projection': '3d'})
 
-the_table = axs.table(cellText=data, colLabels=columns, loc='center')
+# Get the test data
+X, Y, Z = axes3d.get_test_data(0.05)
+
+# Plot the data
+for ax in axs:
+    ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+
+# Set the orthographic projection.
+axs[0].set_proj_type('ortho')  # FOV = 0 deg
+axs[0].set_title("'ortho'\nfocal_length = ∞", fontsize=10)
+
+# Set the perspective projections
+axs[1].set_proj_type('persp')  # FOV = 90 deg
+axs[1].set_title("'persp'\nfocal_length = 1 (default)", fontsize=10)
+
+axs[2].set_proj_type('persp', focal_length=0.2)  # FOV = 157.4 deg
+axs[2].set_title("'persp'\nfocal_length = 0.2", fontsize=10)
 
 plt.show()
