9e2ea59add2b45d6daab96fc9b30240fe240bb00
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-09-21 18:13:17 +0300

    0020: corridor_tv_width.py

---
+++ corridor_tv_width.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from os.path import join
+from util import create_file_list
+
+IN_PATH = '../input/001_cmp_picking'
+OUT_PATH = '../output/001_zomf_corr'
+
+
+def to_zomf_corridor(input_name, output_name, save_header, max_angle, corridor_width, min_width, max_width,
+                     min_velocity):
+    df = pd.read_csv(input_name, sep='\t', skiprows=13, usecols=np.arange(7), dtype=np.float64,
+                     names=save_header.split())
+    max_time = df.iloc[:, 3].max()
+
+    def update_velocity(velocity):
+        if velocity < min_velocity + corridor_width:
+            return min_velocity + corridor_width
+        else:
+            return velocity
+
+    def update_width(time):
+        return min_width + (time / max_time) * (max_width - min_width)
+
+    df.iloc[:, 6] = max_angle
+    df.iloc[:, 5] = df.iloc[:, 3].apply(update_width)
+    df.iloc[:, 4] = df.iloc[:, 3]
+    df.iloc[:, 2] = df.iloc[:, 2].apply(update_velocity)
+    df.iloc[:, 3] = 0
+    df.to_csv(output_name, sep='\t', index=False)
+
+
+def run():
+    save_header = 'XCoord	YCoord	V	A	T	DeltaV	DeltaA'
+    max_angle = 1.55334
+    corridor_width, min_width, max_width = 500, 400, 1500
+    min_velocity = 1500
+    for name in create_file_list(IN_PATH):
+        to_zomf_corridor(join(IN_PATH, name), join(OUT_PATH, name), save_header, max_angle,
+                         corridor_width, min_width, max_width, min_velocity)
+
+
+if __name__ == '__main__':
+    run()
--- sandbox.py
+++ sandbox.py
@@ -1,25 +1,27 @@
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from os.path import join
+from util import create_file_list
+
+IN_PATH = '../input/001_cmp_picking'
+OUT_PATH = '../output/001_zomf_corr'
 
 
-fig, axs = plt.subplots(1, 3, subplot_kw={'projection': '3d'})
+def to_zomf_corridor(input_name, output_name, save_header, max_angle, corridor_width, min_velocity):
+    df = pd.read_csv(input_name, sep='\t', skiprows=13, usecols=np.arange(7), dtype=np.float64,
+                     names=save_header.split())
+    print(df.iloc[:, 3].max())
 
-# Get the test data
-X, Y, Z = axes3d.get_test_data(0.05)
 
-# Plot the data
-for ax in axs:
-    ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+def run():
+    save_header = 'XCoord	YCoord	V	A	T	DeltaV	DeltaA'
+    max_angle = 1.55334
+    corridor_width = 500
+    min_velocity = 1500
+    for name in create_file_list(IN_PATH):
+        to_zomf_corridor(join(IN_PATH, name), join(OUT_PATH, name), save_header, max_angle, corridor_width,
+                         min_velocity)
 
-# Set the orthographic projection.
-axs[0].set_proj_type('ortho')  # FOV = 0 deg
-axs[0].set_title("'ortho'\nfocal_length = ∞", fontsize=10)
 
-# Set the perspective projections
-axs[1].set_proj_type('persp')  # FOV = 90 deg
-axs[1].set_title("'persp'\nfocal_length = 1 (default)", fontsize=10)
-
-axs[2].set_proj_type('persp', focal_length=0.2)  # FOV = 157.4 deg
-axs[2].set_title("'persp'\nfocal_length = 0.2", fontsize=10)
-
-plt.show()
+if __name__ == '__main__':
+    run()
